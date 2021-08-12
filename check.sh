793cda6a293cda28c92b7f390352028e50d7c7c6
Josh Soref <jsoref@users.noreply.github.com>
2021-08-12 13:35:15 -0400

    Use xargs -P instead of parallel

--- check.sh
+++ check.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-command -v "parallel" >/dev/null 2>/dev/null || (
-  sudo apt-get update
-  sudo apt-get install -y parallel
-)
 dhall_haskell="$(eval echo "$DHALL_BINARY")"
 curl -Ls $dhall_haskell | tar -xjf -
 PATH="$(pwd)/bin:$PATH"
@@ -18,7 +14,7 @@ if [ -n "$FILES" ]; then
   echo "$FILES" | tr "\n" "\0" >> $LIST
 fi
 cat $LIST |
-  parallel -0 --no-notice --no-run-if-empty -n1 $GITHUB_ACTION_PATH/dhall-checker
+  xargs -0 -P2 -r -n1 $GITHUB_ACTION_PATH/dhall-checker
 (
   cd $DHALL_FAILURES
   files="$(find . -type f)"
